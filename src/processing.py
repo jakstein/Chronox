8a01c3938ebfd1f3c990a7599ce72e1db0a891ff
jakstein <180436403+jakstein@users.noreply.github.com>
2025-02-11 10:48:21 +0100

    add extra features for feature engineering

---
+++ src/processing.py
@@ -0,0 +1,48 @@
+import pandas
+import numpy
+
+def addFeatures(filePath):
+    data = pandas.read_csv(filePath, header=0, parse_dates=True)
+
+    data["priceChange"] = data["Close"].pct_change()*100
+    data["ma10"] = data["Close"].rolling(window=10).mean()
+    data["ma50"] = data["Close"].rolling(window=50).mean()
+    
+    data["ema10"] = data["Close"].ewm(span=10, adjust=False).mean()
+    data["ema50"] = data["Close"].ewm(span=50, adjust=False).mean()
+    data["ema12"] = data["Close"].ewm(span=12, adjust=False).mean()
+    data["ema26"] = data["Close"].ewm(span=26, adjust=False).mean()
+
+    data["macd"] = data["ema12"] - data["ema26"]
+    data["macdSignal"] = data["macd"].ewm(span=9, adjust=False).mean()
+    data = data.drop("ema12", axis=1)
+    data = data.drop("ema26", axis=1)
+
+    data["volitStd1w"] = data["Close"].pct_change().rolling(window=7).std()
+    data["volitStd1mo"] = data["Close"].pct_change().rolling(window=30).std()
+
+    data["ma20"] = data["Close"].rolling(window=20).mean()
+    data["bollingerUp"] = data["ma20"] + (data["Close"].rolling(20).std()*2)
+    data["bollingerDown"] = data["ma20"] - (data["Close"].rolling(20).std()*2)
+    data = data.drop("ma20", axis=1)
+
+    data["volma10"] = data["Volume"].rolling(10).mean()
+
+    data["OBV"] = (numpy.sign(data["Close"].diff()) * data["Volume"]).where(data["Close"].diff() != 0).fillna(0).cumsum() #doublecheck, weird results
+
+    RSIdelta = data["Close"].diff()
+    RSIgain = RSIdelta.where(RSIdelta > 0,0)
+    RSIloss = -RSIdelta.where(RSIdelta < 0,0)
+    #wilders smoothing
+    RSIavgGain = RSIgain.ewm(com=13, adjust=False).mean()
+    RSIavgLoss = RSIloss.ewm(com=13, adjust=False).mean()
+    RSI = RSIavgGain/RSIavgLoss
+    data["RSI"] = 100-(100/(1+RSI))
+
+    #add sentiment analysis results here
+
+    data = data.fillna(0) #ensure no empty values
+    data.to_csv(filePath, index=False)
+
+
+addFeatures(".\\data\\processed\\AAPL_5y_1d.csv")
